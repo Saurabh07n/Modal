383c3a01d8199df5c81c8582bc1db24ba4e2b63a
saurabh-zngit <s.n@ambientetechnologies.com>
2022-08-31 20:20:56 +0530

    test

--- script.js
+++ script.js
@@ -12,16 +12,17 @@ const openModal = function () {
 };
 
 const closeModal = function () {
+  console.log('Closing Modal');
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
 for (let i = 0; i < btnShowModal.length; i++) {
   btnShowModal[i].addEventListener('click', openModal);
-
-  btnCloseModal.addEventListener('click', closeModal);
-  overlay.addEventListener('click', closeModal);
 }
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
